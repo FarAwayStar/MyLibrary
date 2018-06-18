5b4dd0c112eba1226721036a2b28faba66c5871a
10261 <10261@DESKTOP-IKTFLER>
2018-06-18 11:43:09 +0800

    添加项目文件。

---
+++ MyLibrary/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace MyLibrary
+{
+    partial class dlu
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cancleButton = new System.Windows.Forms.Button();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.codeTextBox = new System.Windows.Forms.TextBox();
+            this.codeLabel = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cancleButton
+            // 
+            this.cancleButton.Location = new System.Drawing.Point(284, 362);
+            this.cancleButton.Margin = new System.Windows.Forms.Padding(4);
+            this.cancleButton.Name = "cancleButton";
+            this.cancleButton.Size = new System.Drawing.Size(100, 29);
+            this.cancleButton.TabIndex = 11;
+            this.cancleButton.Text = "取消";
+            this.cancleButton.UseVisualStyleBackColor = true;
+            this.cancleButton.Click += new System.EventHandler(this.cancleButton_Click);
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(83, 362);
+            this.loginButton.Margin = new System.Windows.Forms.Padding(4);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(100, 29);
+            this.loginButton.TabIndex = 10;
+            this.loginButton.Text = "登录";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // codeTextBox
+            // 
+            this.codeTextBox.Location = new System.Drawing.Point(194, 283);
+            this.codeTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.codeTextBox.Name = "codeTextBox";
+            this.codeTextBox.PasswordChar = '*';
+            this.codeTextBox.Size = new System.Drawing.Size(200, 25);
+            this.codeTextBox.TabIndex = 9;
+            // 
+            // codeLabel
+            // 
+            this.codeLabel.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.codeLabel.Location = new System.Drawing.Point(88, 283);
+            this.codeLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.codeLabel.Name = "codeLabel";
+            this.codeLabel.Size = new System.Drawing.Size(133, 26);
+            this.codeLabel.TabIndex = 8;
+            this.codeLabel.Text = "密码：";
+            this.codeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(194, 225);
+            this.nameTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(200, 25);
+            this.nameTextBox.TabIndex = 7;
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.nameLabel.Location = new System.Drawing.Point(88, 225);
+            this.nameLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(133, 26);
+            this.nameLabel.TabIndex = 6;
+            this.nameLabel.Text = "帐号：";
+            this.nameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // dlu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(471, 423);
+            this.Controls.Add(this.cancleButton);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.codeTextBox);
+            this.Controls.Add(this.codeLabel);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.nameLabel);
+            this.Name = "dlu";
+            this.Text = "登录";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cancleButton;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.TextBox codeTextBox;
+        private System.Windows.Forms.Label codeLabel;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Label nameLabel;
+    }
+}
+
---
+++ MyLibrary/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace MyLibrary
+{
+    public partial class dlu : Form
+    {
+        public dlu()
+        {
+            InitializeComponent();
+        }
+
+        DBAccess user = new DBAccess();
+        SqlConnection sqlcon = DBAccess.GetConnection();//建立连接
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            string SQLstr = "select * from dbo.ylduser where 用户ID='" +
+                nameTextBox.Text.Trim() + "'and 密码='" + codeTextBox.Text.Trim() + "'";
+            SqlCommand sqlCommand = sqlcon.CreateCommand();  //创建一个SqlCommand对象，用于执行sql语句
+            sqlCommand.CommandText = SQLstr;                        //获取指定的sql语句
+            SqlDataReader temDR = sqlCommand.ExecuteReader();       //执行sql语句名，生成一个sqldatareader对象
+            bool bReaded = temDR.Read();
+            if (bReaded)
+            {
+                MessageBox.Show("登录成功！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                sqlcon.Close();   //关闭数据库的连接
+                sqlcon.Dispose();   //释放My_con变量的所有空间
+                Hide();
+                //MainWindow sb = new MainWindow();
+                //sb.ShowDialog();
+                Application.Exit();
+            }
+            else
+            {
+                MessageBox.Show("登录失败！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                nameTextBox.Text = "";
+                codeTextBox.Text = "";
+                nameTextBox.Focus();
+            }
+        }
+
+        private void cancleButton_Click(object sender, EventArgs e)
+        {
+            sqlcon.Close();
+            sqlcon.Dispose();
+            Application.Exit();
+        }
+    }
+}
